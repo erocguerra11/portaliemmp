9209b1308595787e92c69a007b48e8561a3e87a2
eric <eric2000gu@gmail.com>
2026-06-05 12:43:59 +0200

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+const usuari = localStorage.getItem('usuari');
+const rol = localStorage.getItem('rol');
+
+if (!usuari) {
+    window.location.href = 'index.html';
+}
+
+document.getElementById('nom-usuari').textContent = 'Usuari: ' + usuari.toUpperCase();
+document.getElementById('rol-usuari').textContent = 'Rol: ' + rol.charAt(0).toUpperCase() + rol.slice(1);
+
+if (rol === 'alumne') {   
+    document.querySelector('.targetes').innerHTML = '<p> No tens accés a cap aplicació.</p>';
+}
